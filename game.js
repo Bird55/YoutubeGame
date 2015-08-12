9d0a83557d04c8169e82cf4ad7561bfe772de35b
Alexey Solovyev <bird55@narod.ru>
2015-08-13 00:16:04 +0300

    Home comit

--- game.js
+++ game.js
@@ -12,14 +12,22 @@ var clearBtn;
 var gameWidth = 800;
 var gameHeight = 500;
 
-var plWidth = 120;
-var plHeight = 70;
-
 var background = new Image();
 background.src = "img/bg.png";
 
 var tiles = new Image();
 tiles.src = "img/tiles.png";
+
+var player;
+var enemy;
+
+var requestAnimFrame = window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame;
+
+var isPlaying;
 
 function init() {
     map = document.getElementById("map");
@@ -31,8 +39,8 @@ function init() {
     pl = document.getElementById("player");
     ctxPl = pl.getContext("2d");
 
-    pl.width = plWidth;
-    pl.height = plHeight;
+    pl.width = gameWidth;
+    pl.height = gameHeight;
 
     drawBtn = document.getElementById("drawBtn");
     clearBtn = document.getElementById("clearBtn");
@@ -40,8 +48,74 @@ function init() {
     drawBtn.addEventListener("click", drawRect, false);
     clearBtn.addEventListener("click", clearRect, false);
 
+    player = new Player();
+    enemy = new Enemy();
+
     drawBg();
-    drawPlayer();
+
+    startLoop();
+//    draw();
+}
+
+function loop() {
+    if (isPlaying) {
+        draw();
+        update();
+        requestAnimFrame(loop);
+    }
+}
+
+function startLoop() {
+    isPlaying = true;
+    loop();
+}
+
+function stopLoop() {
+    isPlaying = false;
+}
+
+function draw() {
+    player.draw();
+    enemy.draw();
+}
+
+function update() {
+
+}
+
+// Objects
+function Player() {
+    this.srcX = 0;
+    this.srcY = 0;
+    this.drawX = 0;
+    this.drawY = 0;
+    this.width = 120;
+    this.height = 70;
+
+    this.speed = 8;
+}
+
+function Enemy() {
+    this.srcX = 0;
+    this.srcY = 70;
+    this.drawX = 700;
+    this.drawY = 50;
+    this.width = 100;
+    this.height = 70;
+
+    this.speed = 5;
+}
+
+Player.prototype.draw = function () {
+    ctxMap.drawImage(tiles,
+        this.srcX, this.srcY, this.width, this.height,
+        this.drawX, this.drawY, this.width, this.height);
+}
+
+Enemy.prototype.draw = function() {
+    ctxMap.drawImage(tiles,
+        this.srcX, this.srcY, this.width, this.height,
+        this.drawX, this.drawY, this.width, this.height);
 }
 
 function drawRect() {
@@ -56,9 +130,4 @@ function clearRect() {
 function drawBg() {
     ctxMap.drawImage(background, 0, 0, 800, 480,
         0, 0, gameWidth, gameHeight);
-}
-
-function drawPlayer() {
-    ctxMap.drawImage(tiles, 0, 0, 120, 70,
-        0, 0, plWidth, plHeight);
 }
